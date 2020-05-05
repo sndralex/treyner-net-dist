55ad1e7a11b9ad2d1327545db769bf97d84a9976
Alexander Treyner <alexander.treyner@gmail.com>
2020-05-05 11:50:01 +0300

    --

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -12,19 +12,18 @@
         </div>
       </div-->
     </div>
-    <div class=" section-team text-center">
+    <div class="section-team text-center">
       <div class="container">
         <h2 class="title">{{homeData.title}}</h2>
+        <h5 class="description">
+              <RichTextElement
+                    v-if="homeData.memoElement.resolveHtml()"
+                    class="article-detail-content"
+                    :element="homeData.memoElement"
+                />
+            </h5>
         <div class="team">
-          <div
-              v-if="resumes.length === 0"
-              class="row"
-          >
-          </div>
-          <div 
-            v-else 
-            class="row"
-          >
+          <div class="row">
           <template  v-for="(resume) in homeData.resumesMap">
            <div :key="resume.id" class="col-md-3">
               <div class="team-player">
@@ -58,28 +57,10 @@
         </div>
       </div>
     </div>
-    <div class="section section-about-us">
+    <div class="section">
       <div class="container">
-        <div class="row">
-          <div class="col-md-8 ml-auto mr-auto text-center">
-            <h2 class="title">{{homeData.title}}</h2>
-            <h5 class="description">
-              <RichTextElement
-                    v-if="homeData.memoElement.resolveHtml()"
-                    class="article-detail-content"
-                    :element="homeData.memoElement"
-                />
-            </h5>
-          </div>
-        </div>
-        <div class="separator separator-primary"></div>
-        <div class="section-story-overview">
-          <div class="row">
-            <div class="col-md-6">
-              <div
-                class="image-container1 image-left1"
-                
-              >
+          <div class="row justify-content-end">
+            <div class="col-md-2 team-player">
               <img
                   v-if="homeData.wwordImage"
                     :alt="homeData.wwordSignature"
@@ -87,62 +68,17 @@
                     :src="homeData.wwordImage"
                     :title="homeData.wwordSignature"
                 />
+            </div>
+            <div class="col-md-6">
                 <!-- First image on the left side -->
                 <p class="blockquote blockquote-primary">
                   <RichTextElement
                     v-if="homeData.wwordElement.resolveHtml()"
-                    class="article-detail-content"
                     :element="homeData.wwordElement"
                 />
-                  <br />
-                  <br />
                   <small>{{homeData.wwordSignature}}</small>
                 </p>
-              </div>
-              <!-- Second image on the left side of the article -->
-              <div
-                class="image-container"
-                style="background-image: url('img/bg3.jpg')"
-              ></div>
             </div>
-            <div class="col-md-5">
-              <!-- First image on the right side, above the article -->
-              <div
-                class="image-container image-right"
-                style="background-image: url('img/bg1.jpg')"
-              ></div>
-              <h3>
-                So what does the new record for the lowest level of winter ice
-                actually mean
-              </h3>
-              <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
-              </p>
-              <p>
-                For a start, it does not automatically follow that a record
-                amount of ice will melt this summer. More important for
-                determining the size of the annual thaw is the state of the
-                weather as the midnight sun approaches and temperatures rise.
-                But over the more than 30 years of satellite records, scientists
-                have observed a clear pattern of decline, decade-by-decade.
-              </p>
-              <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </div>

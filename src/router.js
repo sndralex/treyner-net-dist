e320774c70e9a64650bdfdb8d6eafd8f373293f9
Alexander Treyner <alexander.treyner@gmail.com>
2020-05-16 20:02:20 +0300

    cancelled History mode

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ import { onErrorResumeNext } from 'rxjs';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  //mode: 'history',
   linkExactActiveClass: 'active',
   routes: [
     {
